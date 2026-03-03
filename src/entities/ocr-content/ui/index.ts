619d974193da826b76930595aae514e9d4e9bb3a
hyeonseo1109 <alsgustj2@naver.com>
2026-03-04 05:05:43 +0900

    refactor: 안 쓰는 컴포넌트 삭제

--- src/entities/ocr-content/ui/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ImagePreview } from './ImagePreview.vue'
-export { default as ResultViewer } from './ResultViewer.vue'
--- src/pages/main/mainPage.vue
+++ src/pages/main/mainPage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 
 import '@/app/styles/style.css'
-// import { OcrBoard } from '@/widgets/ocr-board/ui'
 import { OcrView } from '@/features/copy-text/ui'
 import * as styles from './style.css'
 </script>
@@ -9,6 +8,5 @@ import * as styles from './style.css'
 <template>
 <div :class="styles.container">
   <OcrView />
-  <!-- <OcrBoard />  -->
 </div>
 </template>
