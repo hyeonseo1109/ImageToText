fb734f9dedb2389baae0f4ec2fbd457319355836
hyeonseo1109 <alsgustj2@naver.com>
2026-03-04 03:01:07 +0900

    style: app style 정렬 수정

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 
 import '@/app/styles/style.css'
-import MainPage from '@/pages/main/mainPage.vue';
+import MainPage from '@/pages/main/mainPage.vue'
+import * as styles from './style.css'
+
 </script>
 
 <template>
-  <main>
+  <main :class="styles.main">
     <MainPage />
     </main>
 </template>
